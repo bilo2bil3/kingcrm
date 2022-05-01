06eedf30ccf158506f3e24be275081425346a56a
bilo2bil3 <momen.medhat.official@gmail.com>
2022-05-01 17:31:34 +0200

    add password field for create_agents form

--- agents/forms.py
+++ agents/forms.py
@@ -6,11 +6,26 @@ User = get_user_model()
 
 
 class AgentModelForm(forms.ModelForm):
+    password=forms.CharField(widget=forms.PasswordInput())
+    confirm_password=forms.CharField(widget=forms.PasswordInput())
+
     class Meta:
         model = User
         fields = (
             'email',
             'username',
             'first_name',
-            'last_name'
+            'last_name',
+            'password',
+            'confirm_password'
         )
+
+    def clean(self):
+        cleaned_data = super(AgentModelForm, self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "password and confirm_password does not match"
+            )
--- agents/views.py
+++ agents/views.py
@@ -28,18 +28,18 @@ class AgentCreateView(OrganisorAndLoginRequiredMixin, generic.CreateView):
         user = form.save(commit=False)
         user.is_agent = True
         user.is_organisor = False
-        user.set_password(f"{random.randint(0, 1000000)}")
+        user.set_password(user.password)
         user.save()
         Agent.objects.create(
             user=user,
             organisation=self.request.user.userprofile
         )
-        send_mail(
-            subject="You are invited to be an agent",
-            message="You were added as an agent on DJCRM. Please come login to start working.",
-            from_email="admin@test.com",
-            recipient_list=[user.email]
-        )
+        # send_mail(
+        #     subject="You are invited to be an agent",
+        #     message="You were added as an agent on DJCRM. Please come login to start working.",
+        #     from_email="admin@test.com",
+        #     recipient_list=[user.email]
+        # )
         return super(AgentCreateView, self).form_valid(form)
 
 
