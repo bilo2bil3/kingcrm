26dec7be02e7f3c9e1c3bcfdc2c52dc395a96335
bilo2bil3 <98341649+bilo2bil3@users.noreply.github.com>
2022-05-08 00:26:25 +0300

    Update run_load_leads_from_gsheets.sh

--- leads/management/commands/run_load_leads_from_gsheets.sh
+++ leads/management/commands/run_load_leads_from_gsheets.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $HOME/kingcrm
 source $HOME/.local/share/virtualenvs/kingcrm-wDqBJNVH/bin/activate
-export GOOGLE_API_KEY=AIzaSyBgmTksVWTTuAfAbc46XcR9E0wjlvtUM_g
+export GOOGLE_API_KEY=AIzaSyDu3ba8tAbu9vUm57tS3LmpTZ5xFTaqU8s
 export READ_DOT_ENV_FILE=True
 python manage.py load_leads_from_gsheets
