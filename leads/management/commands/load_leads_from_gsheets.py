3cd823ffb38f26928e60c92ab3588bbe489abe66
mazenalhabeby <a.mazen.dessouky@gmail.com>
2022-07-11 19:25:28 +0200

    commit

--- leads/management/commands/load_leads_from_gsheets.py
+++ leads/management/commands/load_leads_from_gsheets.py
@@ -14,14 +14,31 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         pass
 
-    # function to get the data from google sheets
-    def get_data(self, sheet_id, sheet_range):
-        url = self.ENDPOINT_URL.format(sheet_id, sheet_range)
-        response = requests.get(url, params=self.PARAMS)
-        return response.json()
+    def handle(self, *args, **options):
+        for sheet in LeadsSheet.objects.all():
+            sheet_id = sheet.url.split('d/')[-1].split('/')[0]
+            sheet_range = f'{sheet.sheet_name}!A:I'
 
+            url = self.ENDPOINT_URL.format(sheet_id, sheet_range)
+            r = requests.get(url, params=self.PARAMS)
+            if r.status_code != 200:
+                raise Exception(f'connect to gsheets api | ERROR | {r.status_code}')
 
-       
+            rows = r.json().get('values', [])[1:]
+
+            # to skip duplicate leads while uploading
+            # we need to keep track of leads added for current session
+            leads_to_add = []
+            for row in rows:
+                if row[5] + row[6] in [lead[5] + lead[6] for lead in leads_to_add]:
+                    continue
+                leads_to_add.append(row)
+            print(sheet.random)
+            if sheet.random:
+                add_leads(assign_randomly=True, organisation=sheet.organisation, leads_to_add=leads_to_add)
+            else:
+                add_leads(organisation=sheet.organisation, leads_to_add=leads_to_add, agent=sheet.agent)
+            # for lead in leads_to_add:
             #     [first_name, last_name, source, service, email, country_code, phone_number, country, campaign] = lead
 
             #     phone_number = country_code + phone_number
